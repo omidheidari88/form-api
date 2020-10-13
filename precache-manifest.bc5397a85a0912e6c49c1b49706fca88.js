849d02e74aa48ea74bf1de0d6eb36f239d33d4c6
Omid Heidari <omid_heidari90@yahoo.com>
2020-10-13 19:44:44 +0330

    Updates

---
+++ precache-manifest.bc5397a85a0912e6c49c1b49706fca88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8757c715a0f9eff34ef6dbfd78ee6cb",
+    "url": "/form-api/index.html"
+  },
+  {
+    "revision": "2f5b5116fe9127899049",
+    "url": "/form-api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "39f8f51bed96447c15a2",
+    "url": "/form-api/static/js/2.d51cf91f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form-api/static/js/2.d51cf91f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5b5116fe9127899049",
+    "url": "/form-api/static/js/main.fef1226e.chunk.js"
+  },
+  {
+    "revision": "c0a1b4cc31620acfe07e",
+    "url": "/form-api/static/js/runtime-main.9a13a852.js"
+  }
+]);
